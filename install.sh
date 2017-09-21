4865fe9e2d49c23a472e6eb7cc77c0cbdc6e98ce
Kozlov Vitali <kozlov_vitali@hotmail.com>
2017-09-21 13:17:46 +0300

    refactoring installer

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# MAIN CONSTS
 CUR_DIR="./"
 LOG_DIR="/tmp/tag_checker_log/"
 LINK_PATH="/usr/local/bin/"
@@ -26,21 +27,8 @@ PREFIX_F="#!/bin/bash"
 PYTHON="python3"
 GIT="git"
 
-# cases
-ONE=1
-TWO=2
-THREE=3
-FOUR=4
-FIVE=5
-SIX=6
-SEV=7
-EIGHT=8
-NINE=9
-
-# parameters
+# PARAMETERS VARS
 def_ar=("" "" "" "" "" "")
-sw_on="on"
-sw_off="off"
 
 sud="-s"
 mt="-m"
@@ -52,9 +40,11 @@ log="-l"
 setted_params=("${def_ar[@]}")
 setted_params_sw=("${def_ar[@]}")
 
-# dialogs
-
+# DIALOG VARS
 dialog=(dialog)
+
+sw_on="on"
+sw_off="off"
 
 wnd_sz="8 78"
 wnd_sz_f="20 78 10"
@@ -69,61 +59,15 @@ full_uninst_title=(--title "Full uninstall")
 after_change_pararms_title=(--title "Create files")
 create_dirs_title=(--title "Create dirs")
 
-# check and remove file
-check_and_rem_f() {    
-    if [ -f "$1" ] 
-    then
-        rm -f $1 &> /dev/null
-    fi
+# MAIN FUNCS
+main() {
+    check_soft
+
+    main_menu
+
+    exit 0
 }
 
-# check and remove dir
-check_and_rem_d() {    
-    if [ -d "$1" ] 
-    then
-        rm -rf $1 &> /dev/null
-    fi
-}
-
-# check an make dir
-check_and_make_d() {
-    if [ ! -d "$1" ] 
-    then
-        mkdir -p $1 &> /dev/null
-    fi
-}
-
-# create executable file
-create_exec_file() {
-    touch $SETUP_DIR$EXEC_F
-    
-    chmod +x $SETUP_DIR$EXEC_F
-    
-    echo $PREFIX_F > $SETUP_DIR$EXEC_F
-    echo $SETUP_DIR$NAME "${setted_params[@]}" '$@' >> $SETUP_DIR$EXEC_F
-}
-
-# create link
-create_link() {
-    check_and_rem_f "$LINK_PATH$TAG_CHECKER"
-
-    ln -s $SETUP_DIR$EXEC_F $LINK_PATH$TAG_CHECKER &> /dev/null
-}
-
-#  build ver
-build_ver() {
-    branch=$($GIT rev-parse --abbrev-ref HEAD) &> /dev/null
-    commits=$($GIT rev-list $branch --count) &> /dev/null
-    last_author=$($GIT log -1 --pretty=format:"%an") &> /dev/null
-    last_hash=$($GIT log -1 --pretty=format:"%h") &> /dev/null
-
-    sed -Ei "s/current_commits/$commits/g" $SETUP_DIR$VERSION_FILE  &> /dev/null
-    sed -i "s@beta@$branch@" $SETUP_DIR$VERSION_FILE  &> /dev/null
-    sed -i "s@last_commiter@$last_author@" $SETUP_DIR$VERSION_FILE  &> /dev/null
-    sed -i "s@last_hash@$last_hash@" $SETUP_DIR$VERSION_FILE  &> /dev/null
-}
-
-# check soft installed
 check_soft() {
     # check pyton
     if ! which $PYTHON > /dev/null; then
@@ -138,7 +82,6 @@ check_soft() {
     fi
 }
 
-# main menu
 main_menu() {
     main_menu_dlg=(--clear \
                    --title "Tag Checker Installer" \
@@ -196,6 +139,108 @@ main_menu() {
     clear
 }
 
+full_install() {
+    full_install_dlg=(--gauge "Full install..." $wnd_sz_g)
+    (
+        progress_step "Init installing \n fadas \n asdasd" 5
+        
+        progress_step "Create dirs" 35
+    ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_install_dlg[@]}" 2>&1 >/dev/tty)
+
+    create_dirs
+    
+    (
+        progress_step "Update files" 65
+    ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_install_dlg[@]}" 2>&1 >/dev/tty)
+
+    update_files
+
+    (
+        progress_step "Change parameters" 80
+    ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_install_dlg[@]}" 2>&1 >/dev/tty)
+
+    change_parameters
+    accept_params
+
+    (
+        progress_step "Edit crontab" 90
+    ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_install_dlg[@]}" 2>&1 >/dev/tty)
+
+    edit_crontab
+
+    (
+        progress_step "Finish installing" 100
+    ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_install_dlg[@]}" 2>&1 >/dev/tty)
+    
+    clear
+}
+
+create_dirs() {
+    cr_dirs_dlg=(--gauge "Create dirs" $wnd_sz_g)
+    (
+        progress_step "make $SETUP_DIR" 35
+        check_and_make_d "$SETUP_DIR"
+        chmod +x $SETUP_DIR*
+
+        progress_step "make $OUT_ORD_DIR" 75
+        check_and_make_d "$OUT_ORD_DIR"
+        chmod +x $OUT_ORD_DIR*
+
+        progress_step "make $LOG_DIR" 100
+        check_and_make_d "$LOG_DIR"
+        chmod +x $LOG_DIR*
+    ) | $("${dialog[@]}" "${progress_dlg[@]}" "${cr_dirs_dlg[@]}" 2>&1 >/dev/tty)
+
+    clear
+}
+
+full_uninstall() {
+    full_uninstall_dlg=(--gauge "Full uninstall..." $wnd_sz_g)
+    (
+        progress_step "Init uninstalling" 5
+        
+        progress_step "Remove dirs" 40
+    ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_uninstall_dlg[@]}" 2>&1 >/dev/tty)
+
+    delete_dirs
+
+    (
+        progress_step "Remove files" 70
+    ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_uninstall_dlg[@]}" 2>&1 >/dev/tty)
+
+    check_and_rem_f "$CONFIG_DIR$CONFIG_FILE"
+    check_and_rem_f "$LINK_PATH$TAG_CHECKER"
+
+    remove_from_crontab
+
+    (
+        progress_step "Remove note from crontab" 90
+    ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_uninstall_dlg[@]}" 2>&1 >/dev/tty)
+
+
+    (
+        progress_step "Finish uninstalling" 100
+    ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_uninstall_dlg[@]}" 2>&1 >/dev/tty)
+
+    clear
+}
+
+delete_dirs() {
+    del_dirs_dlg=(--gauge "Create dirs" $wnd_sz_g)
+    (
+        progress_step "remove $SETUP_DIR" 35
+        check_and_rem_d "$SETUP_DIR"
+
+        progress_step "remove $OUT_ORD_DIR" 75
+        check_and_rem_d "$OUT_DIR"
+
+        progress_step "remove $LOG_DIR" 100
+        check_and_rem_d "$LOG_DIR"
+    ) | $("${dialog[@]}" "${progress_dlg[@]}" "${del_dirs_dlg[@]}" 2>&1 >/dev/tty)
+
+    clear
+}
+
 upgrade_files() {
     upgrade_prog=(--gauge "Upgrade files..." $wnd_sz_g)
 
@@ -222,124 +267,50 @@ upgrade_files() {
     show_info "Files was updated."
 }
 
-# remove note from crontab
-remove_from_crontab() {
-    crontab -l | grep -q !"$NAME" > temp &> /dev/null
-    crontab temp &> /dev/null
-    rm temp &> /dev/null
-}
-
-# add note to crontab
-add_to_crontab() {
-    crontab -l > temp &> /dev/null
-    echo "0 * * * * $SETUP_DIR$EXEC_F --update" >> temp &> /dev/null
-    crontab temp &> /dev/null
-    rm temp &> /dev/null
-}
-
-# edit crontab
-edit_crontab() {
-    crontab_menu_dlg=(--clear \
-                      --title "Edit crontab" \
-                      --menu "Actions" $wnd_sz_f)
-
-    e_c_opts=(1 "Add to crontab."
-              2 "Remove from crontab."
-              3 "Skip.")
-
-    clear
-    choise=$("${dialog[@]}" "${crontab_menu_dlg[@]}" "${e_c_opts[@]}" 2>&1 >/dev/tty)
-    clear
-    case $choise in
-        1 ) 
-            remove_from_crontab
-            add_to_crontab
-            show_info "Note to crontab was added"
-            ;;
-        2 ) 
-            remove_from_crontab 
-            show_info "Note from crontab was deleted"
-            ;;
-        3 ) ;;
-    esac
-
-    clear
-}
-
-# update files
 update_files() {
     update_f_prog=(--gauge "Update files..." $wnd_sz_g)
 
     clear
     (
-        progress_step "Init updating files" 0
+        progress_step "Init updating files." 0
 
-        progress_step "Script files was copied" 10
-        yes | cp -rf $SRC_DIR* $SETUP_DIR &> /dev/null
+        progress_step "Script files was copied." 10
+        yes | cp -rf $SRC_DIR* $SETUP_DIR
 
         progress_step "Config file was checked." 15
-        cp -rfn $CUR_DIR$CONFIG_FILE $CONFIG_DIR &> /dev/null
-        chmod 777 $CONFIG_DIR$CONFIG_FILE &> /dev/null
+        cp -rfn $CUR_DIR$CONFIG_FILE $CONFIG_DIR
+        chmod 777 $CONFIG_DIR$CONFIG_FILE
 
-        progress_step "Copy scripts" 20
-        cp $CUR_DIR$SRC_DIR$MISC_DIR$SCRIPTS_FILE $OUT_DIR &> /dev/null
+        progress_step "Copy scripts." 20
+        cp $CUR_DIR$SRC_DIR$MISC_DIR$SCRIPTS_FILE $OUT_DIR
 
-        progress_step "Copy styles" 25
-        cp $CUR_DIR$SRC_DIR$MISC_DIR$STYLE_FILE $OUT_DIR &> /dev/null
+        progress_step "Copy styles." 25
+        cp $CUR_DIR$SRC_DIR$MISC_DIR$STYLE_FILE $OUT_DIR
 
-        progress_step "Create build version" 50
+        progress_step "Create build version." 50
         build_ver
 
-        progress_step "Create exec file" 75
-        create_exec_file
+        progress_step "Accept params." 75
+        accept_params
 
-        progress_step "Create link" 90
-        create_link
-
-        progress_step "Finish updating files" 100
+        progress_step "Finish updating files." 100
     ) | $("${dialog[@]}" "${progress_dlg[@]}" "${update_f_prog[@]}" 2>&1 >/dev/tty)
 
     clear
 }
 
-# delete dirs
-delete_dirs() {
-    del_dirs_dlg=(--gauge "Create dirs" $wnd_sz_g)
-    (
-        progress_step "remove $SETUP_DIR" 35
-        check_and_rem_d "$SETUP_DIR"
+build_ver() {
+    branch=$($GIT rev-parse --abbrev-ref HEAD)
+    commits=$($GIT rev-list $branch --count)
+    last_author=$($GIT log -1 --pretty=format:"%an")
+    last_hash=$($GIT log -1 --pretty=format:"%h")
 
-        progress_step "remove $OUT_ORD_DIR" 75
-        check_and_rem_d "$OUT_DIR"
-
-        progress_step "remove $LOG_DIR" 100
-        check_and_rem_d "$LOG_DIR"
-    ) | $("${dialog[@]}" "${progress_dlg[@]}" "${del_dirs_dlg[@]}" 2>&1 >/dev/tty)
-
-    clear
+    sed -Ei "s/current_commits/$commits/g" $SETUP_DIR$VERSION_FILE
+    sed -i "s@beta@$branch@" $SETUP_DIR$VERSION_FILE
+    sed -i "s@last_commiter@$last_author@" $SETUP_DIR$VERSION_FILE
+    sed -i "s@last_hash@$last_hash@" $SETUP_DIR$VERSION_FILE
 }
 
-# create dirs
-create_dirs() {
-    cr_dirs_dlg=(--gauge "Create dirs" $wnd_sz_g)
-    (
-        progress_step "make $SETUP_DIR" 35
-        check_and_make_d "$SETUP_DIR"
-        chmod +x $SETUP_DIR* > /dev/null 2>&1 &
-
-        progress_step "make $OUT_ORD_DIR" 75
-        check_and_make_d "$OUT_ORD_DIR"
-        chmod +x $OUT_ORD_DIR* > /dev/null 2>&1 &
-
-        progress_step "make $LOG_DIR" 100
-        check_and_make_d "$LOG_DIR"
-        chmod +x $LOG_DIR* > /dev/null 2>&1 &
-    ) | $("${dialog[@]}" "${progress_dlg[@]}" "${cr_dirs_dlg[@]}" 2>&1 >/dev/tty)
-
-    clear
-}
-
-# reset attributes
 change_parameters() {
     changed=("${def_ar[@]}")
     changed_sw=("${def_ar[@]}")
@@ -386,6 +357,98 @@ accept_params() {
     clear
 }
 
+create_exec_file() {
+    touch $SETUP_DIR$EXEC_F
+    
+    chmod +x $SETUP_DIR$EXEC_F
+    
+    echo $PREFIX_F > $SETUP_DIR$EXEC_F
+    echo $SETUP_DIR$NAME "${setted_params[@]}" '$@' >> $SETUP_DIR$EXEC_F
+}
+
+create_link() {
+    check_and_rem_f "$LINK_PATH$TAG_CHECKER"
+
+    ln -s $SETUP_DIR$EXEC_F $LINK_PATH$TAG_CHECKER
+}
+
+edit_crontab() {
+    crontab_menu_dlg=(--clear \
+                      --title "Edit crontab" \
+                      --menu "Actions" $wnd_sz_f)
+
+    e_c_opts=(1 "Add to crontab."
+              2 "Remove from crontab."
+              3 "Skip.")
+
+    clear
+    choise=$("${dialog[@]}" "${crontab_menu_dlg[@]}" "${e_c_opts[@]}" 2>&1 >/dev/tty)
+    clear
+    case $choise in
+        1 ) 
+            remove_from_crontab
+            add_to_crontab
+            show_info "Note to crontab was added"
+            ;;
+        2 ) 
+            remove_from_crontab 
+            show_info "Note from crontab was deleted"
+            ;;
+        3 ) ;;
+    esac
+
+    clear
+}
+
+remove_from_crontab() {
+    crontab -l | grep -q !"$NAME" > temp
+    crontab temp
+    rm temp
+}
+
+add_to_crontab() {
+    crontab -l > temp
+    echo "0 * * * * $SETUP_DIR$EXEC_F --update" >> temp
+    crontab temp
+    rm temp
+}
+
+run_from_source() {
+    change_parameters
+
+    $CUR_DIR$SRC_DIR$NAME "${setted_params[@]}" $quiet $log $sud $mt $deb $tim $UPDATE_A
+
+    show_info "Script from source was finish work."
+}
+
+run_now() {
+    $TAG_CHECKER $UPDATE_A
+
+    show_info "installed script was finish work."
+}
+
+# AUX FUNCS
+check_and_rem_f() {    
+    if [ -f "$1" ] 
+    then
+        rm -f $1
+    fi
+}
+
+check_and_rem_d() {    
+    if [ -d "$1" ] 
+    then
+        rm -rf $1
+    fi
+}
+
+check_and_make_d() {
+    if [ ! -d "$1" ] 
+    then
+        mkdir -p $1
+    fi
+}
+
 show_msg() {
     msgbox_dlg=(--title "Message" --msgbox "$1" $wnd_sz)
     $("${dialog[@]}" "${msgbox_dlg[@]}" 2>&1 >/dev/tty)
@@ -400,15 +463,6 @@ show_info() {
     clear
 }
 
-# run_from_source
-run_from_source() {
-    change_parameters
-
-    $CUR_DIR$SRC_DIR$NAME "${setted_params[@]}" $quiet $log $sud $mt $deb $tim $UPDATE_A &> /dev/null
-
-    show_info "Script from source was finish work."
-}
-
 progress_step() {
         echo $2
 
@@ -419,96 +473,11 @@ progress_step() {
         sleep 0.1
 }
 
-# full install
-full_install() {
-    full_install_dlg=(--gauge "Full install..." $wnd_sz_g)
-    (
-        progress_step "Init installing \n fadas \n asdasd" 5
-        
-        progress_step "Create dirs" 35
-    ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_install_dlg[@]}" 2>&1 >/dev/tty)
-
-    create_dirs
-    
-    (
-        progress_step "Update files" 65
-    ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_install_dlg[@]}" 2>&1 >/dev/tty)
-
-    update_files
-
-    (
-        progress_step "Change parameters" 80
-    ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_install_dlg[@]}" 2>&1 >/dev/tty)
-
-    change_parameters
-    accept_params
-
-    (
-        progress_step "Edit crontab" 90
-    ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_install_dlg[@]}" 2>&1 >/dev/tty)
-
-    edit_crontab
-
-    (
-        progress_step "Finish installing" 100
-    ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_install_dlg[@]}" 2>&1 >/dev/tty)
-    
-    clear
-}
-
-# full uninstall
-full_uninstall() {
-    full_uninstall_dlg=(--gauge "Full uninstall..." $wnd_sz_g)
-    (
-        progress_step "Init uninstalling" 5
-        
-        progress_step "Remove dirs" 40
-    ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_uninstall_dlg[@]}" 2>&1 >/dev/tty)
-
-    delete_dirs
-
-    (
-        progress_step "Remove files" 70
-    ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_uninstall_dlg[@]}" 2>&1 >/dev/tty)
-
-    check_and_rem_f "$CONFIG_DIR$CONFIG_FILE"
-    check_and_rem_f "$LINK_PATH$TAG_CHECKER"
-
-    remove_from_crontab
-
-    (
-        progress_step "Remove note from crontab" 90
-    ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_uninstall_dlg[@]}" 2>&1 >/dev/tty)
-
-
-    (
-        progress_step "Finish uninstalling" 100
-    ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_uninstall_dlg[@]}" 2>&1 >/dev/tty)
-
-    clear
-}
-
-# run installed now
-run_now() {
-    $TAG_CHECKER $UPDATE_A &> /dev/null
-
-    show_info "installed script was finish work."
-}
-
 close_all_dialogs() {
     clear
     exit 0
 }
 
-# ---------------------------------
-# main
-# ---------------------------------
-main() {
-    check_soft
 
-    main_menu
-
-    exit 0
-}
-
+# ENTER POINT
 main
