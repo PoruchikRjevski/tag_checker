6f080b09c15613dda114ebab2a8d76d9a059fde6
Kozlov Vitali <kozlov_vitali@hotmail.com>
2017-09-21 13:43:00 +0300

    fix: installer. out selected parameters

--- install.sh
+++ install.sh
@@ -71,13 +71,13 @@ main() {
 check_soft() {
     # check pyton
     if ! which $PYTHON > /dev/null; then
-        show_msg "Please, install python3 before using $0"
+        show_msg "Please, install python3 before using $0."
         exit 0
     fi
 
     # check git
     if ! which $GIT > /dev/null; then
-        show_msg "Please, install git before using $0"
+        show_msg "Please, install git before using $0."
         exit 0
     fi
 }
@@ -87,8 +87,8 @@ main_menu() {
                    --title "Tag Checker Installer" \
                    --menu "Main menu" $wnd_sz_f)
 
-    m_m_opts=(3 "Full install"
-              4 "Full uninstall"
+    m_m_opts=(3 "Install"
+              4 "Uninstall"
               5 "Upgrade files"
               6 "Change parameters"
               7 "Edit crontab"
@@ -115,7 +115,7 @@ main_menu() {
         6 ) 
             change_parameters
             accept_params
-            show_msg "Parameters accepted. Use link: $TAG_CHECKER."
+            show_msg "Parameters accepted: ${setted_params[*]} . Use link: $TAG_CHECKER."
             clear
             main_menu
             ;;
@@ -140,36 +140,36 @@ main_menu() {
 }
 
 full_install() {
-    full_install_dlg=(--gauge "Full install..." $wnd_sz_g)
+    full_install_dlg=(--gauge "Full install" $wnd_sz_g)
     (
-        progress_step "Init installing \n fadas \n asdasd" 5
+        progress_step "Init installing..." 5
         
-        progress_step "Create dirs" 35
+        progress_step "Creating dirs..." 35
     ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_install_dlg[@]}" 2>&1 >/dev/tty)
 
     create_dirs
     
     (
-        progress_step "Update files" 65
+        progress_step "Updating files..." 65
     ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_install_dlg[@]}" 2>&1 >/dev/tty)
 
     update_files
 
     (
-        progress_step "Change parameters" 80
+        progress_step "Changing parameters..." 80
     ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_install_dlg[@]}" 2>&1 >/dev/tty)
 
     change_parameters
     accept_params
 
     (
-        progress_step "Edit crontab" 90
+        progress_step "Editing crontab..." 90
     ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_install_dlg[@]}" 2>&1 >/dev/tty)
 
     edit_crontab
 
     (
-        progress_step "Finish installing" 100
+        progress_step "Finishing installing..." 100
     ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_install_dlg[@]}" 2>&1 >/dev/tty)
     
     clear
@@ -178,15 +178,15 @@ full_install() {
 create_dirs() {
     cr_dirs_dlg=(--gauge "Create dirs" $wnd_sz_g)
     (
-        progress_step "make $SETUP_DIR" 35
+        progress_step "making $SETUP_DIR ..." 35
         check_and_make_d "$SETUP_DIR"
         chmod +x $SETUP_DIR*
 
-        progress_step "make $OUT_ORD_DIR" 75
+        progress_step "making $OUT_ORD_DIR ..." 75
         check_and_make_d "$OUT_ORD_DIR"
         chmod +x $OUT_ORD_DIR*
 
-        progress_step "make $LOG_DIR" 100
+        progress_step "making $LOG_DIR ..." 100
         check_and_make_d "$LOG_DIR"
         chmod +x $LOG_DIR*
     ) | $("${dialog[@]}" "${progress_dlg[@]}" "${cr_dirs_dlg[@]}" 2>&1 >/dev/tty)
@@ -195,46 +195,47 @@ create_dirs() {
 }
 
 full_uninstall() {
-    full_uninstall_dlg=(--gauge "Full uninstall..." $wnd_sz_g)
+    full_uninstall_dlg=(--gauge "Full uninstall" $wnd_sz_g)
     (
-        progress_step "Init uninstalling" 5
+        progress_step "Uninstalling..." 5
         
-        progress_step "Remove dirs" 40
+        progress_step "Removing dirs..." 40
     ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_uninstall_dlg[@]}" 2>&1 >/dev/tty)
 
     delete_dirs
 
     (
-        progress_step "Remove files" 70
+        progress_step "Removing files..." 70
     ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_uninstall_dlg[@]}" 2>&1 >/dev/tty)
 
     check_and_rem_f "$CONFIG_DIR$CONFIG_FILE"
     check_and_rem_f "$LINK_PATH$TAG_CHECKER"
 
+    (
+        progress_step "Removing note from crontab" 90
+    ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_uninstall_dlg[@]}" 2>&1 >/dev/tty)
+
     remove_from_crontab
 
     (
-        progress_step "Remove note from crontab" 90
-    ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_uninstall_dlg[@]}" 2>&1 >/dev/tty)
-
-
-    (
-        progress_step "Finish uninstalling" 100
+        progress_step "Finishing uninstalling..." 100
     ) | $("${dialog[@]}" "${progress_dlg[@]}" "${full_uninstall_dlg[@]}" 2>&1 >/dev/tty)
 
     clear
 }
 
 delete_dirs() {
-    del_dirs_dlg=(--gauge "Create dirs" $wnd_sz_g)
+    del_dirs_dlg=(--gauge "Remove dirs" $wnd_sz_g)
     (
-        progress_step "remove $SETUP_DIR" 35
+        progress_step "Removing dirs..." 0
+
+        progress_step "removing $SETUP_DIR" 35
         check_and_rem_d "$SETUP_DIR"
 
-        progress_step "remove $OUT_ORD_DIR" 75
+        progress_step "removnig $OUT_ORD_DIR" 75
         check_and_rem_d "$OUT_DIR"
 
-        progress_step "remove $LOG_DIR" 100
+        progress_step "removing $LOG_DIR" 100
         check_and_rem_d "$LOG_DIR"
     ) | $("${dialog[@]}" "${progress_dlg[@]}" "${del_dirs_dlg[@]}" 2>&1 >/dev/tty)
 
@@ -242,11 +243,11 @@ delete_dirs() {
 }
 
 upgrade_files() {
-    upgrade_prog=(--gauge "Upgrade files..." $wnd_sz_g)
+    upgrade_prog=(--gauge "Upgrade files" $wnd_sz_g)
 
     clear
     (
-        progress_step "Init updating files" 0
+        progress_step "Updating files..." 0
 
         progress_step "Remove $SETUP_DIR" 5
         check_and_rem_d "$SETUP_DIR"
@@ -261,39 +262,39 @@ upgrade_files() {
 
     (
         progress_step "Files was updated" 50
-        progress_step "Finish updating files" 100
+        progress_step "Finishing updating files..." 100
     ) | $("${dialog[@]}" "${progress_dlg[@]}" "${upgrade_prog[@]}" 2>&1 >/dev/tty)
 
     show_info "Files was updated."
 }
 
 update_files() {
-    update_f_prog=(--gauge "Update files..." $wnd_sz_g)
+    update_f_prog=(--gauge "Update files" $wnd_sz_g)
 
     clear
     (
-        progress_step "Init updating files." 0
+        progress_step "Init updating files..." 0
 
-        progress_step "Script files was copied." 10
+        progress_step "Script files was copied..." 10
         yes | cp -rf $SRC_DIR* $SETUP_DIR
 
-        progress_step "Config file was checked." 15
+        progress_step "Config file was checked..." 15
         cp -rfn $CUR_DIR$CONFIG_FILE $CONFIG_DIR
         chmod 777 $CONFIG_DIR$CONFIG_FILE
 
-        progress_step "Copy scripts." 20
+        progress_step "Copy scripts..." 20
         cp $CUR_DIR$SRC_DIR$MISC_DIR$SCRIPTS_FILE $OUT_DIR
 
-        progress_step "Copy styles." 25
+        progress_step "Copy styles..." 25
         cp $CUR_DIR$SRC_DIR$MISC_DIR$STYLE_FILE $OUT_DIR
 
-        progress_step "Create build version." 50
+        progress_step "Create build version..." 50
         build_ver
 
-        progress_step "Accept params." 75
+        progress_step "Accept params..." 75
         accept_params
 
-        progress_step "Finish updating files." 100
+        progress_step "Finishing updating files..." 100
     ) | $("${dialog[@]}" "${progress_dlg[@]}" "${update_f_prog[@]}" 2>&1 >/dev/tty)
 
     clear
@@ -315,12 +316,12 @@ change_parameters() {
     changed=("${def_ar[@]}")
     changed_sw=("${def_ar[@]}")
 
-    c_p_opts=(1 "Run cmd's as sudo." "${setted_params_sw[1]}"
-              2 "Multithreading on." "${setted_params_sw[2]}"
-              3 "Debug out on." "${setted_params_sw[3]}"
-              4 "Timings out." "${setted_params_sw[4]}"
-              5 "Out logs to stdout off." "${setted_params_sw[5]}"
-              6 "Logging on." "${setted_params_sw[6]}")
+    c_p_opts=(1 "ON SUDO" "${setted_params_sw[1]}"
+              2 "ON MULTITHREADing" "${setted_params_sw[2]}"
+              3 "ON DEBUG out" "${setted_params_sw[3]}"
+              4 "ON TIMINGS calc" "${setted_params_sw[4]}"
+              5 "OFF use STDOUT" "${setted_params_sw[5]}"
+              6 "ON LOGging" "${setted_params_sw[6]}")
 
 
     choises=$("${dialog[@]}" "${change_params_dlg[@]}" "${c_p_opts[@]}" 2>&1 >/dev/tty)
@@ -344,13 +345,13 @@ change_parameters() {
 }
 
 accept_params() {
-    accept_params_dlg=(--gauge "Accept params..." $wnd_sz_g)
+    accept_params_dlg=(--gauge "Accept params" $wnd_sz_g)
     (
-        progress_step "Init create addition files" 0
-        progress_step "Create exec file" 50
+        progress_step "Init create addition files..." 0
+        progress_step "Create exec file..." 50
         create_exec_file
 
-        progress_step "Create link" 100
+        progress_step "Create link..." 100
         create_link
     ) | $("${dialog[@]}" "${progress_dlg[@]}" "${accept_params_dlg[@]}" 2>&1 >/dev/tty)
 
@@ -377,9 +378,9 @@ edit_crontab() {
                       --title "Edit crontab" \
                       --menu "Actions" $wnd_sz_f)
 
-    e_c_opts=(1 "Add to crontab."
-              2 "Remove from crontab."
-              3 "Skip.")
+    e_c_opts=(1 "ADD to crontab"
+              2 "REMOVE from crontab"
+              3 "SKIP")
 
     clear
     choise=$("${dialog[@]}" "${crontab_menu_dlg[@]}" "${e_c_opts[@]}" 2>&1 >/dev/tty)
